5d663ba76a879eb2898c394fd0c06e7a8d961430
cavidan <cavidaneken1@gmail.com>
2022-09-03 18:02:16 +0300

    03.09.22

--- src/main/java/jdbc/PreparedStatement01.java
+++ src/main/java/jdbc/PreparedStatement01.java
@@ -9,57 +9,77 @@ public class PreparedStatement01 {
         Statement st = con.createStatement();
 
 
-        //1. Örnek: Prepared statement kullanarak
-        // company adı IBM olan number_of_employees değerini 9999 olarak güncelleyin.
-        //1. adım: prepared statement query olustur
+        //1. Örnek: Prepared statement kullanarak company adı IBM olan number_of_employees değerini 9999 olarak güncelleyin.
 
-        String sql1="update companies set number_of_employees= ? where company=?";
+       //1. Adım:  Prepared statement query'sini oluştur.
+       String sql1 = "UPDATE companies SET number_of_employees = ? WHERE company = ?";
 
-        //2. adım: preparedstatement objesini olustur.
-        PreparedStatement pst1=con.prepareStatement(sql1);
+       //2. Adım: PreparedStatement objesini oluştur.
+       PreparedStatement pst1 = con.prepareStatement(sql1);
 
-        //3. adım: set...() metotları ile ? için deger gir
-        pst1.setInt(1,9999);
-        pst1.setString(2,"IBM");
+       //3. Adım: set...() methodları ile soru işaretleri için değer gir.
+       pst1.setInt(1,9999);
+       pst1.setString(2,"IBM");
 
-        //4. adım: execute query
-        System.out.println(pst1.executeUpdate()+" satir güncellendi");
+       //4. Adım: Execute query
+       int updateRowSayisi = pst1.executeUpdate();
+       System.out.println(updateRowSayisi+" satır güncellendi.");
 
-        String sql2="select * from companies";
-        ResultSet result1=st.executeQuery(sql2);
+       String sql2 = "SELECT * FROM companies";
+       ResultSet result2 =  st.executeQuery(sql2);
 
-        while (result1.next()){
-            System.out.println(result1.getInt(1)+"--"+result1.getString(2)+"--"+result1.getInt(3));
-        }
+       while (result2.next()){
 
-        //Google için değişiklik
-        pst1.setInt(1,15000);
-        pst1.setString(2,"GOOGLE");
-        pst1.executeUpdate();
-        String sql3="select * from companies";
-        ResultSet result2=st.executeQuery(sql3);
-        while (result2.next()){
-            System.out.println(result2.getInt(1)+"--"+result2.getString(2)+"--"+result2.getInt(3));
-
-        }
-
-        //2. Örnek: "SELECT * FROM <table name>" query'sini prepared statement ile kullanın.
-
-        String sql4="select * from ?";
-        PreparedStatement pst2=con.prepareStatement(sql4);
-        pst2.setString(1,"companies");
-        ResultSet result3=pst2.executeQuery(sql4);//SOR SOR SOR:PreparedStatement ile sorgu satirlari alan sorgu yontemleri kullanilamaz. hatası verdi.
-                                                  // ResultSet result2=st.executeQuery(sql3); Kullanımı standart ve zorunlu mu
-        while(result3.next()){
-            System.out.println(result3.getInt(1)+"--"+result3.getString(2)+"--"+result3.getInt(3));
-        }
+           System.out.println(result2.getInt(1)+"--"+result2.getString(2)+"--"+result2.getInt(3));
+       }
 
 
+       //Google için değişiklik
+       pst1.setInt(1,15000);
+       pst1.setString(2,"GOOGLE");
+
+       int updateRowSayisi2 = pst1.executeUpdate();
+       System.out.println(updateRowSayisi2+" satır güncellendi.");
+
+       String sql3 = "SELECT * FROM companies";
+       ResultSet result3 =  st.executeQuery(sql3);
+
+       while (result3.next()){
+
+           System.out.println(result3.getInt(1)+"--"+result3.getString(2)+"--"+result3.getInt(3));
+       }
+
+       //2. Örnek: "SELECT * FROM <table name>" query'sini prepared statement ile kullanın.
+
+       System.out.println("==================");
+       read_data(con,"countries");
 
 
 
+   }
 
+   //Bir tablonun istenilen datasını prepared statement ile çağırmak için kullanılan method.
+   public static void read_data(Connection con, String tableName ){
+
+       try {
+
+           String query = String.format("SELECT * FROM %s",tableName);//Format() methodu dinamik String oluşturmak için kullanılır.
+
+           Statement statement = con.createStatement();
+           //SQL query'yi çalıştır.
+           ResultSet rs = statement.executeQuery(query);//Datayı çağırıp ResultSet konteynırına koyuyoruz.
+
+           while (rs.next()){//Tüm datayı çağıralım.
+
+               System.out.println(rs.getString(1) + " - " + rs.getString(2) + " - " + rs.getInt(3));
+
+           }
+
+       }catch (Exception e){
+           System.out.println(e);
+       }
 
 
     }
 }
+
